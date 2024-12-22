70764013fa31c19c47ac2a6679278bccaf30aaca
Laure Kpan <laurekpan23@gmail.com>
2024-12-22 00:32:57 -0600

    update bucket name

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.9"
   backend "s3" {
-    # bucket = "uc-d4cg-terraform-state-s3-backend"
+    # bucket = "uc-d4cg-project-terraform-state-s3-bucket"
     # key    = "lambda-cron/terraform.tfstate"
     # region = var.aws_region
   }
